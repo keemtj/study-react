e4b934a8a7327e64bc803aa38317de46f102e910
keemtj <keemgreat@gmail.com>
2020-05-07 15:33:08 +0900

    eslint test

--- cra-step2/src/App2.js
+++ cra-step2/src/App2.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 class App2 extends Component {
   render() {
     const title = 'title';
+    const a = 1;
+    a = 2;
     return <div>{title}</div>;
   }
 }
