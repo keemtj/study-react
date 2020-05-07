c307ab8f4496e278eda850c49aa65061b2db821d
keemtj <keemgreat@gmail.com>
2020-05-07 23:02:34 +0900

    react login form

--- cra-step2/src/App.js
+++ cra-step2/src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import classNames from 'classnames/bind';
+import style from './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+console.log(Component);
+
+const st = classNames.bind(style);
+
+class App extends Component {
+  // TODO: class 공부하기
+  // class field에 string 할당
+  // class field에 function 할당
+  // class field에 Arrow function 할당
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('로그인 시도');
+  };
+
+  handleIdChange = () => {
+    console.log('사용자 아이디 입력중');
+  };
+
+  handlePwChange = () => {
+    console.log('사용자 패스워드 입력중');
+  };
+
+  render() {
+    return (
+      <div className={st('wrap')}>
+        <h1>로그인</h1>
+        <form
+          className={st('formStyle')}
+          onSubmit={this.handleSubmit}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div>
+            <h2>ID: </h2>
+            <input
+              type="text"
+              className={st('inputStyle')}
+              onChange={this.handleIdChange}
+            />
+          </div>
+          <div>
+            <h2>PASS: </h2>
+            <input
+              type="password"
+              className={st('inputStyle')}
+              onChange={this.handlePwChange}
+            />
+          </div>
+          <br />
+          <button
+            className={st('buttonStyle')}
+            type="submit"
+          >
+            로그인하기
+          </button>
+          <button
+            type="button"
+            onClick={this.handleSubmit}
+            name="hello"
+            value="s"
+          >
+            click
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
