e6f73e3f784885e7bf458647127cab22de32ebb5
keemtj <keemgreat@gmail.com>
2020-05-07 16:09:30 +0900

    eject test

--- cra-step2/src/App2.js
+++ cra-step2/src/App2.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class App2 extends Component {
   render() {
     const title = 'title';
-    return <div>{title}</div>;
+    return (
+      <div>
+        <div>{title}</div>;
+      </div>
+    );
   }
 }
 
