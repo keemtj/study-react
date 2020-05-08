d17c484b43c3ad275ffd0e3a086d0006a22d1e09
keemtj <keemgreat@gmail.com>
2020-05-08 11:47:56 +0900

    Function component & Atomic Design

---
+++ cra-step3/src/atoms/button/Button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button = () => (
+  <button type="submit">로그인하기</button>
+);
+
+export default Button;
---
+++ cra-step3/src/atoms/login/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => (
+  <div>
+    <h1>ID:</h1>
+    <input type="text" />
+    <h1>PASS:</h1>
+    <input type="password" />
+  </div>
+);
+
+export default Login;
