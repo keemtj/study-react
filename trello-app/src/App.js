933ff40e032d40ea876f6fb78b7f8673c0a23d6a
keemtj <keemgreat@gmail.com>
2020-05-11 17:27:06 +0900

    delete components

--- trello-app/src/App.js
+++ trello-app/src/App.js
@@ -1,24 +1,8 @@
 import React, { useState } from 'react';
 import TrelloPage from './Components/Pages/TrelloPage';
 
-function App() {
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      userId: 'test1',
-      userPw: '1234',
-      online: false,
-    },
-    {
-      id: 2,
-      userId: 'test2',
-      userPw: '1234',
-      online: false,
-    },
-  ]);
-  console.log(setUsers);
-  return <TrelloPage users={users} />;
-}
+const App = () => {
+  return;
+};
 
 export default App;
-// trello app이 시작하면 logintemplate먼저 보여준다 => user의 online: false
