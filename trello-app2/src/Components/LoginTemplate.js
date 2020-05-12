2aaf35840f316a9b1525958ca1c604f6d88a517c
keemtj <keemgreat@gmail.com>
2020-05-12 12:05:43 +0900

    fix typo

--- trello-app2/src/Components/LoginTemplate.js
+++ trello-app2/src/Components/LoginTemplate.js
@@ -2,11 +2,20 @@ import React from 'react';
 import Header from './Header';
 import LoginForm from './LoginForm';
 
-const LoginTemplate = ({ headerState, inputs, onChange, userCheck }) => {
+const LoginTemplate = ({
+  headerState,
+  inputs,
+  onChange,
+  userCheck,
+}) => {
   return (
     <>
       <Header headerState={headerState} />
-      <LoginForm inputs={inputs} onChange={onChange} userCheck={userCheck} />
+      <LoginForm
+        inputs={inputs}
+        onChange={onChange}
+        userCheck={userCheck}
+      />
     </>
   );
 };
