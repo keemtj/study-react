f0613ca46cee0dc9f83c5738296e9b7cb86b8dce
keemtj <keemgreat@gmail.com>
2020-05-20 17:52:32 +0900

    reducer

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Info from './Info';
 // import Counter from './Counter';
 
+// const App = () => {
+//   const [visible, setVisible] = useState(false);
+//   return (
+//     <div>
+//       <button onClick={() => setVisible(!visible)}>
+//         {visible ? '숨기기버튼' : '보이기버튼'}
+//       </button>
+//       {visible && <Info />}
+//     </div>
+//   );
+// };
+
 const App = () => {
-  const [visible, setVisible] = useState(false);
   return (
-    <div>
-      <button onClick={() => setVisible(!visible)}>
-        {visible ? '숨기기버튼' : '보이기버튼'}
-      </button>
-      {visible && <Info />}
-    </div>
+    <>
+      {/* <Counter /> */}
+      <Info />
+    </>
   );
 };
 
--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,66 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
+
+const initialState = {
+  name: '',
+  nickname: '',
+};
+
+function reducer(state, action) {
+  console.log('[reducer]', state);
+  // action = target
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-
-  useEffect(() => {
-    console.log('1. effect');
-    console.log(name);
-    return () => {
-      console.log('1. cleanup!');
-      console.log(
-        '1. 뒷정리 함수가 호출되면 업데이트 되기 직전 값만 보여줍니다',
-        name === '' ? '""' : `"${name}"`,
-      );
-    };
-  });
-
-  useEffect(() => {
-    console.log('2. 빈 deps 포함할 때 effect');
-    console.log(name);
-    return () => {
-      console.log('2. 빈 deps 포함할 때 cleanup!');
-      console.log(
-        `2. 빈 deps를 포함하면 렌더링이 처음 한번만 일어나므로 value가 업데이트 되어도 cleanup시 업데이트 되기 직전 값인 name: '${name} '만 보여줍니다`,
-        // name === '' ? '""' : `"${name}"`,
-      );
-    };
-  }, []);
-
-  useEffect(() => {
-    console.log('3. deps에 상태값을 넣었을 때 effect');
-    console.log(name);
-    return () => {
-      console.log('3. deps에 상태값을 넣었을 때 cleanup!');
-      console.log(
-        `3. deps에 상태값(name)을 넣었을 때 deps의 상태 변화를 감지하고 name: '${name}'을 보여줍니다`,
-        // name === '' ? '""' : `"${name}"`,
-      );
-    };
-  }, [name]);
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
+  // const [state, f dispatch] = useReducer(f reducer, default value of f reducer === initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { name, nickname } = state;
+  console.log('[Info]', state);
+  const onChange = ({ target }) => {
+    dispatch(target);
   };
 
   return (
     <div>
       <div>
-        <input type="text" value={name} onChange={onChangeName} />
-        <input type="text" value={nickname} onChange={onChangeNickname} />
+        <input type="text" name="name" value={name} onChange={onChange} />
+        <input
+          type="text"
+          name="nickname"
+          value={nickname}
+          onChange={onChange}
+        />
       </div>
       <div>
-        <div>
-          <b>name: </b> {name}
-        </div>
-        <div>
-          <b>nickname: </b> {nickname}
-        </div>
+        <b>이름: </b> {name}
+      </div>
+      <div>
+        <b>닉네임: </b> {nickname}
       </div>
     </div>
   );
