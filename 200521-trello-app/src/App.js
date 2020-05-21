ef04983ae5d59a631ca6c2cb68b9988df774ca0d
keemtj <keemgreat@gmail.com>
2020-05-21 17:20:20 +0900

    React-trello-app

---
+++ 200521-trello-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginSample from './Component/LoginSample';
+import Header from './Component/Header';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <LoginSample />
+    </>
+  );
+};
+
+export default App;
---
+++ 200521-trello-app/src/Component/LoginSample.js
@@ -0,0 +1,63 @@
+import React, { useState, useRef } from 'react';
+
+const LoginSample = () => {
+  const [inputs, setInputs] = useState({
+    id: '',
+    password: '',
+  });
+  const { id, password } = inputs;
+  const inputEl = useRef(null);
+
+  const onChange = ({ target }) => {
+    console.log(`사용자 ${target.placeholder} 입력중`);
+    setInputs({
+      ...inputs,
+      [target.name]: target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    console.log('로그인 시도');
+    e.preventDefault();
+    onReset();
+    inputEl.current.focus();
+  };
+
+  const onReset = () => {
+    setInputs({
+      ...inputs,
+      id: '',
+      password: '',
+    });
+  };
+
+  return (
+    <div>
+      <h1>로그인</h1>
+      <form onSubmit={onSubmit}>
+        <div>ID:</div>
+        <input
+          type="text"
+          name="id"
+          placeholder="아이디"
+          value={id}
+          onChange={onChange}
+          ref={inputEl}
+        />
+        <div>PASS:</div>
+        <input
+          type="password"
+          name="password"
+          placeholder="패스워드"
+          value={password}
+          onChange={onChange}
+        />
+        <div>
+          <button type="submit">로그인하기</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginSample;
