6bfcce13f99e97b43c622e8f4bb476a8c30ea1b4
keemtj <keemgreat@gmail.com>
2020-05-23 23:08:13 +0900

    news-viewer All news Api & categories

---
+++ news-viewer/src/components/NewsList.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+const sampleArticle = {
+  title: 'title',
+  description: 'description',
+  url: 'https://google.com',
+  urlToImage: 'https://via.placeholder.com/160',
+};
+
+const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const url =
+          '/v2/top-headlines?country=kr&apiKey=55754f86787948fe9950a59caadd409e';
+        const response = await axios.get(url);
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <NewsListBlock>대기 중 ...</NewsListBlock>;
+  }
+
+  if (!articles) {
+    console.log(articles);
+    return null;
+  }
+
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
