827a786946e8a3c619f66fa934aa985f88579f7e
keemtj <keemgreat@gmail.com>
2020-05-31 16:35:05 +0900

    conf: react-virtualized

--- todo-app/src/Components/TodoList.js
+++ todo-app/src/Components/TodoList.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import TodoListItem from './TodoListItem';
+import { List } from 'react-virtualized';
 import './TodoList.scss';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
+        <TodoListItem
+          todo={todo}
+          key={key}
+          onRemove={onRemove}
+          onToggle={onToggle}
+          style={style}
+        ></TodoListItem>
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
   return (
-    <div className="TodoList">
+    <>
+      <List
+        className="TodoList"
+        width={512}
+        height={513}
+        rowCount={todos.length}
+        rowHeight={57}
+        rowRenderer={rowRenderer}
+        list={todos}
+        style={{ outline: 'none' }}
+      />
+      {/* <div className="TodoList">
       {todos.map((todo) => (
         <TodoListItem
           todo={todo}
@@ -13,8 +40,11 @@ const TodoList = ({ todos, onRemove, onToggle }) => {
           onToggle={onToggle}
         />
       ))}
-    </div>
+    </div> */}
+    </>
   );
 };
 
-export default TodoList;
+// App 컴포넌트(부모컴포넌트)의 state가 추가되어 리렌더링되면 자식도 불필요한 리렌더링이 일어난다
+// React.memo를 사용해서 미리 사전에 불필요한 리렌더링을 방지한다
+export default React.memo(TodoList);
