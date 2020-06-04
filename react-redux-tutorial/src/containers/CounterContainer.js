e66f7a3a936405d97127b6018b042178c8f1a4a6
keemtj <keemgreat@gmail.com>
2020-06-04 23:07:54 +0900

    useSelector, useDispatch

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 // import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
-  console.log('CounterContainer', number);
+// const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  // useSelector hooks, mapStateToProps 함수와 비슷
+  const number = useSelector((state) => state.counter.number);
+  // useDispatch hooks, mapDispatchToProps 함수같은 기능
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
@@ -33,17 +40,25 @@ const CounterContainer = ({ number, increase, decrease }) => {
 
 // mapStateToProps함수와 mapDispatchToProps함수를
 // connet함수 내부에 익명함수 형태로 선언 후 export
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  // (dispatch) =>
-  //   bindActionCreators(
-  //     {
-  //       increase,
-  //       decrease,
-  //     },
-  //     dispatch,
-  //   ),
-  { increase, decrease },
-)(CounterContainer);
+
+// // connect
+// export default connect(
+//   (state) => ({
+//     number: state.counter.number,
+//   }),
+
+//   // (dispatch) =>
+//   //   bindActionCreators(
+//   //     {
+//   //       increase,
+//   //       decrease,
+//   //     },
+//   //     dispatch,
+//   //   ),
+
+//   // // bindActionCreators 대신 객체로 바로 전달 가능
+//   { increase, decrease },
+// )(CounterContainer);
+
+// connect 대신 useSelector Hooks 사용
+export default CounterContainer;
--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -11,8 +11,7 @@ import rootReducer from './modules';
 // store 생성
 const store = createStore(rootReducer, composeWithDevTools());
 
-console.log(store.getState().counter);
-console.log(store.getState().todos);
+console.log(store.getState());
 ReactDOM.render(
   // Provider에 반드시 store를 props로 전달한다
   <Provider store={store}>
