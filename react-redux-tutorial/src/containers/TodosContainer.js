5699c2cb5d2ccae16dff91579cfe63a136e80388
keemtj <keemgreat@gmail.com>
2020-06-05 15:04:31 +0900

    useActions

--- react-redux-tutorial/src/containers/TodosContainer.js
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,34 +1,52 @@
+// import React, { useCallback } from 'react';
 import React from 'react';
-import { connect } from 'react-redux';
+// import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+// import { connect } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
+import useActions from '../lib/useActions';
 
-const TodosContainer = ({
-  input,
-  todos,
-  changeInput,
-  insert,
-  toggle,
-  remove,
-}) => {
-  console.log('TodosContainer', input);
-  console.log('TodosContainer', todos);
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }));
+  console.log(input);
+  console.log(todos);
+
+  // const dispatch = useDispatch();
+  // const onChangeInput = useCallback((input) => dispatch(changeInput(input)), [
+  //   dispatch,
+  // ]);
+  // const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  // const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+  // const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    [],
+  );
+
   return (
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onRemove={remove}
-      onToggle={toggle}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
     />
   );
 };
 
-export default connect(
-  ({ todos }) => ({
-    input: todos.input,
-    todos: todos.todos,
-  }),
-  { changeInput, insert, toggle, remove },
-)(TodosContainer);
+// export default connect(
+//   ({ todos }) => ({
+//     input: todos.input,
+//     todos: todos.todos,
+//   }),
+//   { changeInput, insert, toggle, remove },
+// )(TodosContainer);
+
+// 지금의 코드는 TodosContainer의 부모 컴포넌트인 App 컴포넌트가 리렌더링되는 일이 없으므로 불필요한 성능 최적화
+export default React.memo(TodosContainer);
