4b398e8a7bdc46542ed6fbad3158d4f705e50341
keemtj <keemgreat@gmail.com>
2020-06-15 05:21:28 +0900

    select, throttle

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,66 +1,69 @@
 import { handleActions, createAction } from 'redux-actions';
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga/effects';
+// import { call, put, takeLatest } from 'redux-saga/effects';
 import * as api from '../lib/api';
 // import createRequestThunk from '../lib/createRequestThunk';
-import { startLoading, finishLoading } from './loading';
+import createRequestSaga from '../lib/createRequestSaga';
 
 // 액션 타입을 선언합니다
 // 한 요청당 세 개를 만들어야 합니다
 
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
-const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
+// const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
-const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
+// const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 export const getPost = createAction(GET_POST, (id) => id);
 export const getUsers = createAction(GET_USERS);
 
-function* getPostSaga(action) {
-  yield put(startLoading(GET_POST)); // 로딩 시작
-  // 파라미터로 action을 받아 오면 액션의 정보를 조회할 수 있습니다.
-  try {
-    // call을 사용하면 Promise를 반환하는 함수를 호출하고, 기다릴 수 있습니다.
-    // 첫 번째 파라미터는 함수, 나머지 파라미터는 해당 함수에 넣을 인수입니다.
-    // api.getPost(action.payload)를 의미
-    const post = yield call(api.getPost, action.payload);
-    yield put({
-      type: GET_POST_SUCCESS,
-      payload: post.data,
-    });
-  } catch (e) {
-    yield put({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    });
-  }
-  yield put(finishLoading(GET_POST)); // 로딩 완료
-}
+const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+// function* getPostSaga(action) {
+//   yield put(startLoading(GET_POST)); // 로딩 시작
+//   // 파라미터로 action을 받아 오면 액션의 정보를 조회할 수 있습니다.
+//   try {
+//     // call을 사용하면 Promise를 반환하는 함수를 호출하고, 기다릴 수 있습니다.
+//     // 첫 번째 파라미터는 함수, 나머지 파라미터는 해당 함수에 넣을 인수입니다.
+//     // api.getPost(action.payload)를 의미
+//     const post = yield call(api.getPost, action.payload);
+//     yield put({
+//       type: GET_POST_SUCCESS,
+//       payload: post.data,
+//     });
+//   } catch (e) {
+//     yield put({
+//       type: GET_POST_FAILURE,
+//       payload: e,
+//       error: true,
+//     });
+//   }
+//   yield put(finishLoading(GET_POST)); // 로딩 완료
+// }
 
-function* getUserSaga(action) {
-  yield put(startLoading(GET_USERS));
-  try {
-    const users = yield call(api.getUsers, action.payload);
-    yield put({
-      type: GET_USERS_SUCCESS,
-      payload: users.data,
-    });
-  } catch (e) {
-    yield put({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true,
-    });
-  }
-  yield put(finishLoading(GET_USERS));
-}
+const getUsersSaga = createRequestSaga(GET_USERS, api.getUsers);
+// function* getUserSaga(action) {
+//   yield put(startLoading(GET_USERS));
+//   try {
+//     const users = yield call(api.getUsers, action.payload);
+//     yield put({
+//       type: GET_USERS_SUCCESS,
+//       payload: users.data,
+//     });
+//   } catch (e) {
+//     yield put({
+//       type: GET_USERS_FAILURE,
+//       payload: e,
+//       error: true,
+//     });
+//   }
+//   yield put(finishLoading(GET_USERS));
+// }
 
 export function* sampleSaga() {
   yield takeLatest(GET_POST, getPostSaga);
-  yield takeLatest(GET_USERS, getUserSaga);
+  yield takeLatest(GET_USERS, getUsersSaga);
 }
 
 // thunk 함수를 생성합니다
