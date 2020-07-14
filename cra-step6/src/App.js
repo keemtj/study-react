44d62cebc95382cdf26e9c5d1a804ddc251c14fb
keemtj <keemgreat@gmail.com>
2020-07-14 22:20:07 +0900

    :recycle: Refactoring App.js code

--- cra-step6/src/App.js
+++ cra-step6/src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import MainRouter from './Router/MainRouter';
 import Header from './Page/Header';
 
 function App() {
-  const activeStyle = {
-    background: '#000',
-    color: '#fff',
-  };
-
   return (
     <div>
       <Header />
