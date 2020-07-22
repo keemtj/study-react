a4666c07b629ba0b64438c99290dd11f30193ce7
keemtj <keemgreat@gmail.com>
2020-07-22 19:58:26 +0900

    :construction: Fix useState to useReducer

--- ch4-api-intergrate/src/Users.js
+++ ch4-api-intergrate/src/Users.js
@@ -1,42 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
 
+// type: LOADING, SUCCESS, ERROR
+function reducer(state, action) {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case 'SUCCESS':
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case 'ERROR':
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function Users() {
-  /*
-    - useState와 useEffect로 데이터 로딩하기
-    - 3가지 상태 관리
-      1. 요청의 결과
-      2. 로딩 상태
-      3. 에러
-   */
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false); // api가 요청중인지 아닌지 상태를 나타낸다
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
 
-  // 컴포넌트가 처음 렌더링 될 때 어떤 작업을 하겠다
-  // useEffect(() => {}, []);
+  const fetchUsers = async () => {
+    // 로딩이 시작할 때
+    dispatch({ type: 'LOADING' });
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users/',
+      );
+      dispatch({ type: 'SUCCESS', data: response.data });
+    } catch (e) {
+      dispatch({ type: 'ERROR', error: e });
+    }
+  };
+
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        // 초기화 작업
-        setUsers(null);
-        setError(null);
-
-        // 데이터를 가져오는 동안 loading = true
-        setLoading(true);
-        const response = await axios.get(
-          'https://jsonplaceholder.typicode.com/users',
-        );
-
-        // 가져온 데이터를 setUsers를 통해 업데이트
-        setUsers(response.data);
-      } catch (e) {
-        setError(e);
-      }
-      // 데이터를 가져오고 새롭게 users를 업데이트 해주면 loading = false
-      setLoading(false);
-    };
-    // fetchUsers 호출
     fetchUsers();
   }, []);
 
@@ -45,18 +56,30 @@ function Users() {
     -> Trotting: Online(default)을 Fast 3G, Slow 3G로 설정
     -> loading, error일 때의 return값 확인 가능
   */
+  const { loading, data: users, error } = state;
+
   if (loading) return <div>로딩중 ... </div>;
-  if (error) return <div>에러가 발생했습니다</div>;
+  if (error)
+    return (
+      <>
+        <div>에러가 발생했습니다</div>
+        <button onClick={fetchUsers}>다시 불러오기</button>
+      </>
+    );
   if (!users) return null; // return null; =>  아무것도 안보여지게 됨
 
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.username} ({user.name})
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      {/* 데이터 다시 불러오기 버튼 */}
+      <button onClick={fetchUsers}>다시 불러오기</button>
+    </>
   );
 }
 
