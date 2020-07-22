95dcf121b00376354c68e88166cda3f0a493da52
keemtj <keemgreat@gmail.com>
2020-07-22 20:32:53 +0900

    :construction: Apply useAsync.js to Users.js

--- ch4-api-intergrate/src/Users.js
+++ ch4-api-intergrate/src/Users.js
@@ -1,70 +1,30 @@
-import React, { useEffect, useReducer } from 'react';
+import React from 'react';
 import axios from 'axios';
+import useAsync from './useAsync';
 
-// action: LOADING, SUCCESS, ERROR
-// 리듀서함수가 생기면서 코드가 길어졌지만 따로 파일분리가 가능하다
-function reducer(state, action) {
-  switch (action.type) {
-    case 'LOADING':
-      return {
-        loading: true,
-        data: null,
-        error: null,
-      };
-    case 'SUCCESS':
-      return {
-        loading: false,
-        data: action.data,
-        error: null,
-      };
-    case 'ERROR':
-      return {
-        loading: false,
-        data: null,
-        error: action.error,
-      };
-    default:
-      throw new Error(`Unhandled action type: ${action.type}`);
-  }
+// f getUsers: 데이터를 불러오는 함수 = api를 불러오는 함수
+async function getUsers() {
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/users/',
+  );
+  return response.data;
 }
 
 function Users() {
-  const [state, dispatch] = useReducer(reducer, {
-    loading: false,
-    data: null,
-    error: null,
-  });
-
-  const fetchUsers = async () => {
-    // 로딩이 시작할 때
-    dispatch({ type: 'LOADING' });
-    try {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/users/',
-      );
-      dispatch({ type: 'SUCCESS', data: response.data });
-    } catch (e) {
-      dispatch({ type: 'ERROR', error: e });
-    }
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
+  // custom hook, useAsync(callback, deps =[])
+  const [state, refetch] = useAsync(getUsers, []);
   /*
     Web(chrome) inspector -> network tab
     -> Trotting: Online(default)을 Fast 3G, Slow 3G로 설정
     -> loading, error일 때의 return값 확인 가능
   */
   const { loading, data: users, error } = state;
-
   if (loading) return <div>로딩중 ... </div>;
   if (error)
     return (
       <>
         <div>에러가 발생했습니다</div>
-        <button onClick={fetchUsers}>다시 불러오기</button>
+        <button onClick={refetch}>다시 불러오기</button>
       </>
     );
   if (!users) return null; // return null; =>  아무것도 안보여지게 됨
@@ -79,7 +39,7 @@ function Users() {
         ))}
       </ul>
       {/* 데이터 다시 불러오기 버튼 */}
-      <button onClick={fetchUsers}>다시 불러오기</button>
+      <button onClick={refetch}>다시 불러오기</button>
     </>
   );
 }
