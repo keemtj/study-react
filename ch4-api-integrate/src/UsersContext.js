576a9b1a3348255f4cbff3cc4a901d8ef4788a43
keemtj <keemgreat@gmail.com>
2020-07-23 17:43:38 +0900

    :recycle: Split reducer

--- ch4-api-integrate/src/UsersContext.js
+++ ch4-api-integrate/src/UsersContext.js
@@ -1,42 +1,14 @@
 import React, { createContext, useReducer, useContext } from 'react';
 import * as api from './api';
-import createAsyncDispatcher from './asyncActionUtils';
+import createAsyncDispatcher, {
+  initialAsyncState,
+  createAsyncHandler,
+} from './asyncActionUtils';
 
 const initialState = {
-  // loading: loading state
-  // data: 결과값
-  // error: error
-  // users api 요청한 결과를 담을 예정
-  users: {
-    loading: false,
-    data: null,
-    error: null,
-  },
-  // 특정 user를 담을 예정
-  user: {
-    loading: false,
-    data: null,
-    error: null,
-  },
+  users: initialAsyncState,
+  user: initialAsyncState,
 };
-
-const loadingState = {
-  loading: true,
-  data: null,
-  error: null,
-};
-
-const success = (data) => ({
-  loading: false,
-  data,
-  error: null,
-});
-
-const error = (e) => ({
-  loading: false,
-  data: null,
-  error: e,
-});
 
 // reducer
 // GET_USERS
@@ -45,38 +17,19 @@ const error = (e) => ({
 // GET_USER
 // GET_USER_SUCCESS
 // GET_USER_ERROR
+const usersHandler = createAsyncHandler('GET_USERS', 'users');
+const userHandler = createAsyncHandler('GET_USER', 'user');
+
 function usersReducer(state, action) {
   switch (action.type) {
     case 'GET_USERS':
-      return {
-        ...state,
-        users: loadingState,
-      };
     case 'GET_USERS_SUCCESS':
-      return {
-        ...state,
-        users: success(action.data),
-      };
     case 'GET_USERS_ERROR':
-      return {
-        ...state,
-        users: error(action.error),
-      };
+      return usersHandler(state, action);
     case 'GET_USER':
-      return {
-        ...state,
-        user: loadingState,
-      };
     case 'GET_USER_SUCCESS':
-      return {
-        ...state,
-        user: success(action.data),
-      };
     case 'GET_USER_ERROR':
-      return {
-        ...state,
-        user: error(action.error),
-      };
+      return userHandler(state, action);
     default:
       throw new Error('Unhandled action type', action.type);
   }
--- ch4-api-integrate/src/asyncActionUtils.js
+++ ch4-api-integrate/src/asyncActionUtils.js
@@ -17,3 +17,63 @@ export default function createAsyncDispatcher(type, promiseFn) {
 
   return actionHandler;
 }
+
+export const initialAsyncState = {
+  // loading: loading state = default = false
+  // data: 결과값 = default = null
+  // error: error = default = null
+  // users api 요청한 결과를 담을 예정
+  loading: false,
+  data: null,
+  error: null,
+};
+
+const loadingState = {
+  loading: true,
+  data: null,
+  error: null,
+};
+
+const success = (data) => ({
+  loading: false,
+  data,
+  error: null,
+});
+
+const error = (e) => ({
+  loading: false,
+  data: null,
+  error: e,
+});
+
+// reducer를 더 편하게 작성할 수 있도록 하는 함수
+// type: action type
+// key: state 안에 들어있는 객체의 key
+export function createAsyncHandler(type, key) {
+  const SUCCESS = `${type}_SUCCESS`;
+  const ERROR = `${type}_ERROR}`;
+
+  function handler(state, action) {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: loadingState,
+        };
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: success(action.data),
+        };
+      case ERROR:
+        return {
+          ...state,
+          [key]: error(action.error),
+        };
+      default:
+        return state;
+    }
+  }
+
+  return handler;
+}
