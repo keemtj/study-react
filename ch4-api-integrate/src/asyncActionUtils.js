99b98c9aec9d0ffc4ca5cfaafc37b92ef181d098
keemtj <keemgreat@gmail.com>
2020-07-23 18:13:13 +0900

    :bulb: Add comments in promiseFn of asyncActionUtils.js

--- ch4-api-integrate/src/asyncActionUtils.js
+++ ch4-api-integrate/src/asyncActionUtils.js
@@ -77,6 +77,8 @@ export function createAsyncHandler(type, key) {
 // api만 요청하는 것이 아니라 api를 요청하기 전에 특정 action을 dispatch하고
 // api가 성공하거나 실패했을 때도 특정 action을 dispatch한다
 // 추후 이 함수를 호출하게 될때 파라미터로 받아와서 사용
+// type: action type
+// promiseFn: api call function
 export default function createAsyncDispatcher(type, promiseFn) {
   const SUCCESS = `${type}_SUCCESS`;
   const ERROR = `${type}_ERROR}`;
