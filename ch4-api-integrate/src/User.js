ef778bfde2e3e6791f41ed9a5df23379f6734da7
keemtj <keemgreat@gmail.com>
2020-07-23 14:58:13 +0900

    :construction: Fix custom hook to react-async lib

--- ch4-api-integrate/src/User.js
+++ ch4-api-integrate/src/User.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import axios from 'axios';
-import useAsync from './useAsync';
+// import useAsync from './useAsync';
+import { useAsync } from 'react-async';
 
-async function getUser(id) {
+async function getUser({ id }) {
   const response = await axios.get(
     `https://jsonplaceholder.typicode.com/users/${id}`,
   );
   return response.data;
 }
+// promiseFn: api 호출 함수
+// id
+// watch: id값이 바뀌면 다시 호출하겠다는 뜻(deps랑 유사)
 function User({ id }) {
-  // [state, fetchData]
-  // callback에 getUser(id) api 호출 함수
-  // deps에는 id -> id가 바뀔 때마다 데이터를 가져오겠다 라는 뜻
-  const [state] = useAsync(() => getUser(id), [id]);
-  const { loading, data: user, error } = state;
+  const { data: user, error, isLoading } = useAsync({
+    promiseFn: getUser,
+    id,
+    watch: id,
+  });
 
-  if (loading) return <div>로딩중 ... </div>;
+  if (isLoading) return <div>로딩중 ... </div>;
   if (error) return <div>에러가 발생했습니다.</div>;
   if (!user) return null;
+
   return (
     <div>
       <h2>{user.username}</h2>
--- ch4-api-integrate/src/Users.js
+++ ch4-api-integrate/src/Users.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import useAsync from './useAsync';
+// import useAsync from './useAsync';
+import { useAsync } from 'react-async';
 import User from './User';
 
 // f getUsers: 데이터를 불러오는 함수 = api를 불러오는 함수
@@ -12,26 +13,27 @@ async function getUsers() {
 }
 
 function Users() {
-  // custom hook, useAsync(callback, deps = [], skip = false)
-  const [state, refetch] = useAsync(getUsers, [], true);
   const [userId, setUserId] = useState(null);
+  const { data: users, error, isLoading, reload, run } = useAsync({
+    // promiseFn: getUsers, // 데이터를 바로 불러옴
+    deferFn: getUsers, // 원할 때 데이터를 불러옴
+  });
   /*
     Web(chrome) inspector -> network tab
     -> Trotting: Online(default)을 Fast 3G, Slow 3G로 설정
     -> loading, error일 때의 return값 확인 가능
   */
-  const { loading, data: users, error } = state;
-  if (loading) return <div>로딩중 ... </div>;
+  if (isLoading) return <div>로딩중 ... </div>;
   if (error)
     return (
       <>
         <div>에러가 발생했습니다</div>
-        <button onClick={refetch}>다시 불러오기</button>
+        <button onClick={reload}>다시 불러오기</button>
       </>
     );
   // data:users가 없는 상황에서 return null; ->  아무것도 안보여지도록 처리
   // if (!users) return null;
-  if (!users) return <button onClick={refetch}>데이터 불러오기</button>; // null -> button 클릭으로 데이터를 불러오도록 설정
+  if (!users) return <button onClick={run}>데이터 불러오기</button>; // null -> button 클릭으로 데이터를 불러오도록 설정
 
   return (
     <>
@@ -43,7 +45,7 @@ function Users() {
         ))}
       </ul>
       {/* 데이터 다시 불러오기 버튼 */}
-      <button onClick={refetch}>다시 불러오기</button>
+      <button onClick={reload}>다시 불러오기</button>
       {/* <li>에서 onClick 이벤트가 발생하면 User 컴포넌트를 보여줌 */}
       {userId && <User id={userId} />}
     </>
