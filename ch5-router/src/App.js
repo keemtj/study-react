9322d8bc7b884f995cdf6a36801bba308009a482
keemtj <keemgreat@gmail.com>
2020-07-29 20:40:31 +0900

    :construction: Add Profile to match.params

--- ch5-router/src/App.js
+++ ch5-router/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Profile from './Profile';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <hr />
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 }
