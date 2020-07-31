d2ed42fc591b59f7f3afa718d2b9abbc7e9df1d3
keemtj <keemgreat@gmail.com>
2020-07-31 16:46:57 +0900

    :construction: Add WithRouterSample.js

--- ch5-router/src/Profile.js
+++ ch5-router/src/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import WithRouterSample from './WithRouterSample';
 
 const profileData = {
   keemtj: {
@@ -27,6 +28,7 @@ function Profile({ match }) {
         {username} ({profile.name})
       </h3>
       <p>{profile.description}</p>
+      <WithRouterSample />
     </div>
   );
 }
