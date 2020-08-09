04db2372b0487ac3cb0800ef46a8b9792a451477
keemtj <keemgreat@gmail.com>
2020-08-09 22:06:27 +0900

    :lipstick: Add button style by color

--- ch2-styling-with-sass/src/components/Button.js
+++ ch2-styling-with-sass/src/components/Button.js
@@ -4,11 +4,12 @@ import classNames from 'classnames';
 import './Button.scss';
 
 // size: large, medium, small
+// color: blue, pink, gray
 // function Button({ children, size = 'medium' }) {
-function Button({ children, size }) {
+function Button({ children, size, color }) {
   return (
     <>
-      <button className={classNames('Button', size)}>{children}</button>
+      <button className={classNames('Button', size, color)}>{children}</button>
       {/* <button className={classNames('Button', size || 'medium')}>{children}</button> */}
       {/* <button className={['Button', size].join(' ')}>{children}</button> */}
       {/* <button className={`Button ${size}`}>{children}</button> */}
@@ -24,6 +25,7 @@ function Button({ children, size }) {
 // Button.defaultProps = { size: 'medium'};
 Button.defaultProps = {
   size: 'medium',
+  color: 'blue',
 };
 
 export default Button;
