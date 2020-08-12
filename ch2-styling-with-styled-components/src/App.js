05fc9cb5508827b060196267c56054455a93cdc9
keemtj <keemgreat@gmail.com>
2020-08-12 17:17:33 +0900

    :bulb: Add comment by css of styled-components

--- ch2-styling-with-styled-components/src/App.js
+++ ch2-styling-with-styled-components/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Circle = styled.div`
   width: 5rem;
   height: 5rem;
-  background: black;
+  background: ${(props) => props.color};
   border-radius: 50%;
+  ${(props) =>
+    props.huge &&
+    // css가 없으면 template literal: 백틱안에 props 불가능
+    // css가 있으면 Tagged template literal: 백틱안에 새로운 props를 넣을 수 있다
+    css`
+      width: 10rem;
+      height: 10rem;
+    `}
 `;
 
 function App() {
