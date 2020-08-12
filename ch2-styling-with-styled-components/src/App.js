9de96dfe58a0a6ed7d01ab72e4349e0cc3edc0cb
keemtj <keemgreat@gmail.com>
2020-08-12 16:58:58 +0900

    :heavy_plus_sign: Add styled-components

--- ch2-styling-with-styled-components/src/App.js
+++ ch2-styling-with-styled-components/src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: black;
+  border-radius: 50%;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Circle color="blue" huge />;
 }
 
 export default App;
