0b43ea8c1a94efaab93e677421e2b744476f1825
keemtj <keemgreat@gmail.com>
2020-09-18 21:13:36 +0900

    :children_crossing: Improve user experience2

--- ch6-learn-redux-middleware/src/containers/PostContainer.js
+++ ch6-learn-redux-middleware/src/containers/PostContainer.js
@@ -1,22 +1,30 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Post from '../components/Post';
-import { clearPost, getPost } from '../modules/posts';
+import { reducerUtils } from '../lib/asyncUtils';
+import { getPost } from '../modules/posts';
 
 // postId: router parameter를 통해 받아올 예정
 const PostContainer = ({ postId }) => {
-  const { data, loading, error } = useSelector((state) => state.posts.post);
+  // const { data, loading, error } = useSelector((state) => state.posts.post);
+  // const { data, loading, error } = useSelector(
+  //   (state) => state.posts.post[postId] || {},
+  // );
+  const { data, loading, error } = useSelector(
+    (state) => state.posts.post[postId] || reducerUtils.initial(),
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
+    // if (data) return; // dispatch 요청조차 하기싫을 때
     dispatch(getPost(postId));
     // cleanup: 컴포넌트가 언마운트 될 때(뒤로가기 혹은 페이지 나가기), 혹은 그럴일은 없겠지만 post의 Id가 바뀌어서 위의 effect 함수가 호출되기 직전에 호출
-    return () => {
-      dispatch(clearPost());
-    };
-  }, [postId, dispatch]);
+    // return () => {
+    //   dispatch(clearPost());
+    // };
+  }, [postId, dispatch, data]);
 
-  if (loading) return <div>loading...</div>;
+  if (loading && !data) return <div>loading...</div>;
   if (error) return <div>error...</div>;
   if (!data) return null;
 
--- ch6-learn-redux-middleware/src/modules/posts.js
+++ ch6-learn-redux-middleware/src/modules/posts.js
@@ -19,18 +19,110 @@ const CLEAR_POST = 'CLAER_POST';
 // action creator 대신에 thunk 작성
 export const getPosts = createPromiseThunk(GET_POSTS, api.getPosts);
 // api를 요청하여 처리하는 thunk 작성
-export const getPost = createPromiseThunk(GET_POST, api.getPostById);
+// export const getPost = createPromiseThunk(GET_POST, api.getPostById);
+
+// ! meta값을 id로 전달해주면 나중에 리듀서에서 이 id를 참고해서 상태를 업데이트 해줄 예정
+export const getPost = (id) => async (dispatch) => {
+  dispatch({ type: GET_POST, meta: id });
+  try {
+    const payload = await api.getPostById(id);
+    dispatch({ type: GET_POST_SUCCESS, payload, meta: id });
+  } catch (e) {
+    dispatch({
+      type: GET_POST_ERROR,
+      payload: e,
+      error: true,
+      meta: id,
+    });
+  }
+};
+
 export const clearPost = () => ({ type: CLEAR_POST });
 
 // initialState
 const initialState = {
   posts: reducerUtils.initial(),
-  post: reducerUtils.initial(),
+  // post: reducerUtils.initial(),
+  post: {},
 };
+/** 기존의 initialState
+ * post의 경우 id에 따른 데이터를 불러오기 때문에
+ * 새로운 데이터를 불러오게 되면 기존 상태에 새로운 상태를 덮어씌우게 된다
+ * 데이터 캐싱이 불가능
+{
+  posts: {
+    data,
+    loading,
+    error,
+  },
+  post: {
+    data,
+    loading,
+    error,
+  },
+}
+*/
+/* 변경된 initialState
+{
+  posts: {
+    data,
+    loading,
+    error,
+  },
+  post: {
+    '1': {
+      data,
+      loading,
+      error
+    },
+    '2': {
+      data,
+      loading,
+      error
+    },
+    [id]: {
+      data,
+      loading,
+      error
+    }
+  },
+}
+*/
 
 // ! 3번째 Parameter로 기존데이터가 존재한다는 true를 넣어준다
 const getPostsReducer = handleAsyncActions(GET_POSTS, 'posts', true);
-const getPostReducer = handleAsyncActions(GET_POST, 'post');
+// const getPostReducer = handleAsyncActions(GET_POST, 'post');
+const getPostReducer = (state, action) => {
+  const id = action.meta;
+  switch (action.type) {
+    case GET_POST:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.loading(state.post[id] && state.post[id].data),
+        },
+      };
+    case GET_POST_SUCCESS:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.success(action.payload),
+        },
+      };
+    case GET_POST_ERROR:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.error(action.payload),
+        },
+      };
+    default:
+      return state;
+  }
+};
 
 // reducer
 const posts = (state = initialState, action) => {
